7b2139be064d3336eb81d1b740ed84a2ded206fc
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-14 22:12:52 +0200

    235 making router more dynamic

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,5 +2,8 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded id : {{ this.$route.params.id }}</p>
+        <router-link tag="button" 
+        :to="'/user/' + this.$route.params.id + '/edit'"
+        class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
