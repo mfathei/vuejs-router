45f06398af75508c2ed1d782de0c23c177a3d307
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-14 20:09:18 +0200

    227 styling active links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <ul class="nav nav-tabs">
-        <li role="presentation"><router-link to="/">Home</router-link></li>
-        <li role="presentation"><router-link to="/user">User</router-link></li>
+    <ul class="nav nav-pills">
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
     </ul>
 </template>
