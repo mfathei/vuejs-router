9760b6c1b11f2875d01b92d11551cdda3a97c0d4
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-14 23:19:12 +0200

    242 passing the hash fragment

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,7 +3,22 @@
         <h3>Some User Details</h3>
         <p>User loaded id : {{ $route.params.id }}</p>
         <router-link tag="button" 
-        :to="{ name: 'editUser', params: { id: $route.params.id}, query: { locale: 'en', q: 100} }"
+        :to="link"
         class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: {
+        name: "editUser",
+        params: { id: this.$route.params.id },
+        query: { locale: "en", q: 100 },
+        hash: "#data"
+      }
+    };
+  }
+};
+</script>
