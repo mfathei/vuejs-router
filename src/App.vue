35cffb402c13fb50db150557d6be926ff1f9d8f2
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-14 23:10:50 +0200

    241 animating route transition

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <router-view name="header-top"></router-view>
-                <router-view></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
                 <router-view name="header-bottom"></router-view>
             </div>
         </div>
@@ -20,6 +22,46 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.slide-enter {
+  opacity: 0;
+}
 
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+
+.slide-leave {
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 3s; /* I set it to 3 s */
+  opacity: 0;
+}
+
+.slide-move {
+  transition: transform 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
